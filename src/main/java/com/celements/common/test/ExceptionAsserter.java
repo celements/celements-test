4c3d4e210a73e41f065046416d0a92bf9aa975af
Marc Sladek <marc.sladek@synventis.com>
2017-04-27 19:54:18 +0200

    improved ExceptionAsserter

--- src/main/java/com/celements/common/test/ExceptionAsserter.java
+++ src/main/java/com/celements/common/test/ExceptionAsserter.java
@@ -1,43 +1,63 @@
 package com.celements.common.test;
 
+import static com.google.common.base.Preconditions.*;
 import static java.text.MessageFormat.format;
 import static org.junit.Assert.*;
 
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
+
 /**
- * asserts that an {@link Exception} is thrown within {@link #execute()}. note that the evaluation
- * is already done in the constructor to assure correct usage.
+ * asserts that an {@link Exception} of type T is thrown within {@link #execute()}. for further
+ * asserts use {@link #getThrownException()}. note that the evaluation is already done while
+ * constructing to assure correct usage.
+ *
+ * @param <T>
  */
-public abstract class ExceptionAsserter {
+public abstract class ExceptionAsserter<T extends Exception> {
 
-  private final Class<? extends Exception> token;
-  private final Exception instance;
+  private final Class<T> token;
+  private final T expected;
   private final String message;
+  private final T exception;
 
-  public ExceptionAsserter(Class<? extends Exception> token, String message) {
-    this.token = token;
-    this.instance = null;
-    this.message = message;
-    evaluate();
+  public ExceptionAsserter(@NotNull Class<T> token) {
+    this(token, "");
   }
 
-  public ExceptionAsserter(Exception exception, String message) {
-    this.token = exception.getClass();
-    this.instance = exception;
-    this.message = message;
-    evaluate();
+  public ExceptionAsserter(@NotNull Class<T> token, @Nullable String message) {
+    this(token, null, message);
   }
 
-  private void evaluate() {
+  public ExceptionAsserter(@NotNull Class<T> token, @Nullable T expected) {
+    this(token, expected, "");
+  }
+
+  public ExceptionAsserter(@NotNull Class<T> token, @Nullable T expected,
+      @Nullable String message) {
+    this.token = checkNotNull(token);
+    this.expected = expected;
+    this.message = message;
+    this.exception = checkNotNull(evaluate());
+  }
+
+  public @NotNull T getThrownException() {
+    return exception;
+  }
+
+  private @NotNull T evaluate() {
     try {
       execute();
       fail(format("expecting [{0}]: {1}", token.getSimpleName(), message));
+      return null; // never happens
     } catch (Exception exc) {
-      if (instance != null) {
-        assertSame(instance, exc);
+      if (expected != null) {
+        assertSame(expected, exc);
       } else {
         assertTrue(format("expected exeception type [{0}] but was [{1}]", token.getSimpleName(),
             exc.getClass().getSimpleName()), exc.getClass().isAssignableFrom(token));
       }
+      return token.cast(exc);
     }
   }
 
