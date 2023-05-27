67a14b84b22fcdc635399de4fdb9f1ac8fb817c2
Marc Sladek <marc@sladek.dev>
2023-05-27 16:04:53 +0200

    rename

--- celements-base-test/src/main/java/com/celements/common/test/TestLazyInitBeanFactoryPostProcessor.java
+++ celements-base-test/src/main/java/com/celements/common/test/TestLazyInitBeanFactoryPostProcessor.java
@@ -1,24 +1,14 @@
 package com.celements.common.test;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.core.Ordered;
 
-/**
- * {@link BeanFactoryPostProcessor} to set lazy-init on bean definitions that are not
- * {@link LazyInitializationExcludeFilter excluded} and have not already had a value
- * explicitly set.
- * <p>
- * Note that {@link SmartInitializingSingleton SmartInitializingSingletons} are
- * automatically excluded from lazy initialization to ensure that their
- * {@link SmartInitializingSingleton#afterSingletonsInstantiated() callback method} is
- * invoked.
- */
-public final class LazyInitBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
+public final class TestLazyInitBeanFactoryPostProcessor
+    implements BeanFactoryPostProcessor, Ordered {
 
   @Override
   public int getOrder() {
